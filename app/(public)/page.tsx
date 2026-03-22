5c0654f74da822005cb033d1d8f8c457db26cbc3
Erick Vazquez <emvazquez.m96@gmail.com>
2026-03-22 17:05:02 -0600

    configuracion y header

---
+++ app/(public)/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata : Metadata = {
+  title: 'Meeti - Inicio',
+  description: ''
+}
+
+export default function Home() {
+
+  return (
+    <>
+      <h1>Inicio</h1>
+    </>
+  );
+}
